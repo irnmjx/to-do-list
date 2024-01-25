e4ccf9431ef153bed216e7ede85130acd7b005e0
irnmjx <irnmjx@gmail.com>
2024-01-25 15:32:32 +0900

    모든 기능 추가

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const inputText = document.querySelector(".input-text");
-const todoList = document.querySelector(".todo-list");
+const todoList = document.querySelector(".list-container");
+const todo = document.querySelector(".todo");
 
 inputText.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    const a = document.createElement("li");
-    a.innerHTML = `<input type="checkbox" id="checkbox"><label for="checkbox">${inputText.value}</label>`;
-    todoList.appendChild(a);
+    const li = document.createElement("li");
+    li.innerHTML = `<div class="todo">${inputText.value}</div>`;
+    todoList.appendChild(li);
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
     inputText.value = "";
+    saveData();
   }
 });
 
-// const deleteButton = document.querySelector("button");
+todoList.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
 
-// deleteButton.addEventListener("click", function () {});
+function saveData() {
+  localStorage.setItem("data", todoList.innerHTML);
+}
+
+function showTask() {
+  todoList.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
